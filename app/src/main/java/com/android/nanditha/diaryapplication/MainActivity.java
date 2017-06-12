11bd7ba6b18206a4d0a43ebf31db357bfb12e4a4
Nanditha <nandynew@gmail.com>
2017-06-12 16:16:30 -0400

    First Commit

---
+++ app/src/main/java/com/android/nanditha/diaryapplication/MainActivity.java
@@ -0,0 +1,110 @@
+package com.android.nanditha.diaryapplication;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText enterText;
+    private Button saveButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        enterText = (EditText) findViewById(R.id.editText);
+        saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!enterText.getText().toString().equals("")){
+
+                    String data= enterText.getText().toString();
+
+                    writeToFile(data);
+                    Toast.makeText(getApplicationContext(),"Your Secrets are saved !!", Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Fill your Diary now!!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        if(readFromFile()!=null){
+            enterText.setText(readFromFile());
+        }
+        else{
+
+        }
+    }
+
+    private void writeToFile(String myData) {
+
+        try {
+            OutputStreamWriter outputStreamWriter =new OutputStreamWriter(openFileOutput("diary.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(myData);
+            outputStreamWriter.close();
+        }
+
+
+     catch (Exception e) {
+            e.printStackTrace();
+         Log.v("MyActivity log",e.toString());
+        }
+
+    }
+
+
+    private String readFromFile(){
+
+        String result="";
+
+        try{
+            InputStream inputStream =openFileInput("diary.txt");
+            if( inputStream !=null){
+                InputStreamReader inputStreamReader =new InputStreamReader(inputStream);
+                BufferedReader bufferedReader =new BufferedReader(inputStreamReader);
+
+                String tempString="";
+                StringBuilder stringBuilder =new StringBuilder();
+
+                while ((tempString =bufferedReader.readLine())!=null){
+
+                    stringBuilder.append(tempString);
+
+
+
+                }
+
+                inputStream.close();
+                result=stringBuilder.toString();
+            }
+
+        }
+        catch (Exception e){
+            Log.v("MyActivity", e.toString());
+        }
+
+
+        return result;
+    }
+}
+
